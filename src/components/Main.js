04659e461e3c7e7c8da11a3e5a8883598b554e5e
Kirill Malskiy <sabbat@mail.ru>
2023-06-21 09:32:09 +0300

    подписал компонент main на CurrentUserContext, и заменил стейт переменные на значение полей из обьекта контекста

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { api } from '../utils/Api';
 import Card from './Card';
+import {CurrentUserContext} from '../context/CurrentUserContext'
+
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
   const [userName, setUserName] = useState('');
   const [userDescription, setUserDescription] = useState('');
   const [userAvatar, setUserAvatar] = useState('');
   const [cards, setcards] = useState([]);
+
+  //подписка на CurrentUserContext
+  const {avatar, name, about} = useContext(CurrentUserContext)
 
   //получение данных  с сервера и изменение стейта пользовательских данных и карточки
   useEffect(() => {
@@ -36,20 +41,20 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
       <section aria-label='Профиль' className='profile'>
         <img
           className='profile__avatar'
-          src={userAvatar}
+          src={avatar}
           alt='аватар'
           onClick={onEditAvatar}
         />
         <div className='profile__info'>
           <div className='profile__wrap'>
-            <h1 className='profile__name'>{userName}</h1>
+            <h1 className='profile__name'>{name}</h1>
             <button
               className='profile__edit-button'
               type='button'
               onClick={onEditProfile}
             ></button>
           </div>
-          <p className='profile__job'>{userDescription}</p>
+          <p className='profile__job'>{about}</p>
         </div>
         <button
           className='profile__add-button'
